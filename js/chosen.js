fa759614f58f6d224a697a9175562be30c37c2d7
Andriy Iun <andriy.yun@gmail.com>
2020-03-31 15:37:52 +0200

    Added parent menu visibility fix

---
+++ js/chosen.js
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * Attaches behaviors for the adjust Chosen module.
+ */
+
+(function($, Drupal, drupalSettings) {
+  'use strict';
+
+  Drupal.behaviors.os2webHjemmesideChosen = {
+    attach: function (context, settings) {
+      $('select#edit-menu-menu-parent').once('chosen').chosen({width: "100%"});
+    }
+  }
+})(jQuery, Drupal, drupalSettings);
