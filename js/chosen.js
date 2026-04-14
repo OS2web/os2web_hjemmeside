e3bda905fba0c97c02a1d52658114a998e0bcc12
Stanislav Kutasevits <stan@bellcom.dk>
2026-04-14 17:00:32 +0300

    D11 compatibility js update

--- js/chosen.js
+++ js/chosen.js
@@ -3,12 +3,21 @@
  * Attaches behaviors for the adjust Chosen module.
  */
 
-(function ($, Drupal, drupalSettings) {
+
+(function (Drupal, once) {
   'use strict';
 
   Drupal.behaviors.os2webHjemmesideChosen = {
-    attach: function (context, settings) {
-      $('select#edit-menu-menu-parent').once('chosen').chosen({width: "100%"});
+    attach(context) {
+      once('os2webHjemmesideChosen', 'select#edit-menu-menu-parent', context)
+          .forEach((element) => {
+            if (typeof window.jQuery !== 'undefined' && typeof window.jQuery.fn.chosen !== 'undefined') {
+              window.jQuery(element).chosen({
+                width: '100%',
+              });
+            }
+          });
     }
-  }
-})(jQuery, Drupal, drupalSettings);
+  };
+
+})(Drupal, once);
